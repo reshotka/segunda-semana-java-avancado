34bd9d686b15629413a3f6d54e05ee26e3bcef80
reshotka <almaseratto@gmail.com>
2019-05-14 22:22:04 -0300

    correção serviceImpl, testado

--- src/main/java/br/com/alexandra/service/impl/BoloServiceImpl.java
+++ src/main/java/br/com/alexandra/service/impl/BoloServiceImpl.java
@@ -2,13 +2,18 @@ package br.com.alexandra.service.impl;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.alexandra.domain.Bolo;
 import br.com.alexandra.model.BoloModel;
 import br.com.alexandra.repository.BoloRepository;
 import br.com.alexandra.service.BoloService;
 
+@Transactional
+@Service
 public class BoloServiceImpl implements BoloService {
 	
 	@Autowired
